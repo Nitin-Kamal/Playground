faa46428540c0fdb8ff27513eec92175e2b3b0e4
aurora <nitinipun2110@gmail.com>
2026-05-14 14:16:33 +0530

    New project

--- scripts.js
+++ scripts.js
@@ -1,97 +1,91 @@
-function loadup() {
-  document.getElementById("body").style.marginLeft = "0px";
-  document.getElementById("body").style.filter = "blur(0px)";
-}
-
-let d = document.getElementById('indicator');
-function homeind() {
-  d.style.marginLeft = '58px';
-  document.getElementById('homem').style.color = 'brown';
-  document.getElementById('dm1m').style.color = 'black';
-  document.getElementById('dm2m').style.color = 'black';
-  document.getElementById('dm3m').style.color = 'black';
-  document.getElementById('dm4m').style.color = 'black';
-}
-function dm1ind() {
-  d.style.marginLeft = '161px';
-  document.getElementById('dm1m').style.color = 'brown';
-  document.getElementById('homem').style.color = 'black';
-  document.getElementById('dm2m').style.color = 'black';
-  document.getElementById('dm3m').style.color = 'black';
-  document.getElementById('dm4m').style.color = 'black';
-} 
-function dm2ind() {
-  d.style.marginLeft = '275px';
-  document.getElementById('dm2m').style.color = 'brown';
-  document.getElementById('dm1m').style.color = 'black';
-  document.getElementById('homem').style.color = 'black';
-  document.getElementById('dm3m').style.color = 'black';
-  document.getElementById('dm4m').style.color = 'black';
-}
-function dm3ind() {
-  d.style.marginLeft = '390px';
-  document.getElementById('dm3m').style.color = 'brown';
-  document.getElementById('dm1m').style.color = 'black';
-  document.getElementById('dm2m').style.color = 'black';
-  document.getElementById('homem').style.color = 'black';
-  document.getElementById('dm4m').style.color = 'black';
-}
-function dm4ind() {
-  d.style.marginLeft = '505px';
-  document.getElementById('dm4m').style.color = 'brown';
-  document.getElementById('dm1m').style.color = 'black';
-  document.getElementById('dm2m').style.color = 'black';
-  document.getElementById('dm3m').style.color = 'black';
-  document.getElementById('homem').style.color = 'black';
-}
-
-function mobmenu() {
-  document.getElementById('navdivmob').style.width = '100%';
-  document.getElementById('homem').style.display = 'grid';
-  document.getElementById('dm1m').style.display = 'grid';
-  document.getElementById('dm2m').style.display = 'grid';
-  document.getElementById('dm3m').style.display = 'grid';
-  document.getElementById('dm4m').style.display = 'grid';
-  document.getElementById('mobmenu').setAttribute('onclick', 'mobmenuc()');
-  document.getElementById('mobmenu').setAttribute('src', 'close.svg');
-  document.getElementById('mobmenu').style.background = 'red';
-  document.getElementById('mobmenu').style.marginLeft = '91%';
-}
-
-function mobmenuc() {
-  document.getElementById('navdivmob').style.width = '0px';
-  document.getElementById('homem').style.display = 'none';
-  document.getElementById('dm1m').style.display = 'none';
-  document.getElementById('dm2m').style.display = 'none';
-  document.getElementById('dm3m').style.display = 'none';
-  document.getElementById('dm4m').style.display = 'none';
-  document.getElementById('mobmenu').setAttribute('onclick', 'mobmenu()');
-  document.getElementById('mobmenu').setAttribute('src', 'menu.svg');
-  document.getElementById('mobmenu').style.background = 'white';
-  document.getElementById('mobmenu').style.marginLeft = '10px';
-}
+let navind = document.getElementById('navind');
+setInterval(() => {
+    let scrollvalue = window.scrollY;
+    console.log(scrollvalue);
+    let x = document.getElementById('navbar');
+    if (scrollvalue >= 50.8) {
+        x.style.position = 'fixed';
+    } else {
+        x.style.position = 'relative';
+    }
+}, 10);
 
 setInterval(() => {
-  console.log(innerWidth);
-  if (innerWidth < 600) {
-    document.getElementById('mobmenu').style.display = 'grid';
-    document.getElementById('indicator').style.display = 'none';
-    document.getElementById('navcon').style.display = 'none';
-    document.getElementById('dm4').style.display = 'none';
-  } else {
-    document.getElementById('mobmenu').style.display = 'none';
-    document.getElementById('mobmenu').setAttribute('src', 'menu.svg');
-    document.getElementById('navdivmob').style.width = '0px';
-    document.getElementById('indicator').style.display = 'flex';
-    document.getElementById('navcon').style.display = 'grid';
-    document.getElementById('dm4').style.display = 'initial';
-    document.getElementById('homem').style.display = 'none';
-    document.getElementById('dm1m').style.display = 'none';
-    document.getElementById('dm2m').style.display = 'none';
-    document.getElementById('dm3m').style.display = 'none';
-    document.getElementById('dm4m').style.display = 'none';
-    document.getElementById('mobmenu').style.marginLeft = '10px';
-    document.getElementById('mobmenu').style.background = 'white';
-    document.getElementById('mobmenu').setAttribute('onclick', 'mobmenu()');
-  }
+    let x = document.getElementById('navbar');
+    let y = window.innerWidth;
+    let d1 = document.getElementById('d1');
+    let d2 = document.getElementById('d2');
+    let d3 = document.getElementById('d3');
+    let d4 = document.getElementById('d4');
+    x.style.width = (0.8789*y) + 'px';
+    x.style.left = (0.0586*y)+'px';
+
+    if (y > 500) {
+        d1.style.left = (0.2279*y) + 'px';
+        d2.style.left = (0.3385*y) + 'px';
+        d3.style.left = (0.4492*y) + 'px';
+        d4.style.left = (0.5599*y) + 'px';
+    }
+}, 10);
+
+setInterval(() => {
+    let scrollvalue1 = window.scrollY;
+    let d1 = document.getElementById('d1');
+    let x = parseFloat(getComputedStyle(d1).left);
+    let d2 = document.getElementById('d2');
+    let y = parseFloat(getComputedStyle(d2).left);
+    let d3 = document.getElementById('d3');
+    let p = parseFloat(getComputedStyle(d3).left);
+    let d4 = document.getElementById('d4');
+    let q = parseFloat(getComputedStyle(d4).left);
+
+    if (scrollvalue1 <= 1080) {
+        navind.style.width = 0;
+        navind.style.height = 0;
+        navind.style.border = 'none';
+    }
+    if (scrollvalue1 >= 1080 && scrollvalue1 < 2160) {
+        navind.style.width = '80px';
+        navind.style.height = '7px';
+        navind.style.border = '1px solid white';
+        navind.style.left = (x-4) + 'px';
+    }
+    if (scrollvalue1 >= 2160 && scrollvalue1 < 3240) {
+        navind.style.left = (y-4) + 'px';
+    }
+    if (scrollvalue1 >= 3240 && scrollvalue1 < 4320) {
+        navind.style.left = (p-4) + 'px';
+    }
+    if (scrollvalue1 >= 4320) {
+        navind.style.left = (q-4) + 'px';
+    }
 }, 100);
+
+
+function dum1() {
+    window.scrollTo({top: 1080, behavior: "smooth"});
+    let d1 = document.getElementById('d1');
+    let x = d1.getAttribute(style, left);
+    navind.style.left = (x-4) + 'px';
+}
+
+function dum2() {
+    window.scrollTo({top: 2160, behavior: "smooth"});
+    let d2 = document.getElementById('d2');
+    let y = d2.getAttribute(style, left);
+    navind.style.left = (y-4) + 'px';
+}
+
+function dum3() {
+    window.scrollTo({top: 3240, behavior: "smooth"});
+    let d3 = document.getElementById('d3');
+    let p = d3.getAttribute(style, left);
+    navind.style.left = (p-4) + 'px';
+}
+
+function dum4() {
+    window.scrollTo({top: 4320, behavior: "smooth"});
+    let d4 = document.getElementById('d4');
+    let q = d4.getAttribute(style, left);
+    navind.style.left = (q-4) + 'px';
+}
